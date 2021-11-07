7c376611a47195566445b5cfbbcb7eccd0820097
Carloski <carlosmtamayo@gmail.com>
2021-11-06 22:11:03 -0400

    Added Functionality
    Added Bitly API and fixed changed invisible class from "Display: none" to "visibility: hidden"

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-//Testing Prism
-function helloWorld(){
-    alert('Hello World!');
+let btnSubmit = document.getElementById("show-btn").addEventListener("click", getValues);
+let btnClear = document.getElementById("clear-btn").addEventListener("click", clearForm);
+let myForm = document.getElementById("myForm");
+let baseUrl = "developer.nps.gov/api/v1/parks?stateCode=";
+let state = document.getElementById("state").val;
+let limit = document.getElementbyId("numResults").val;
+let key = "1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV";
+
+//curl -X GET "https://developer.nps.gov/api/v1/parks?stateCode=CA&limit=10&api_key=1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV" -H  "accept: application/json"
+
+function clearForm() {
+    myForm.reset();
 }
+
+function getValues() {
+    fetch(`${baseUrl}${state}&limit=${limit}&api_key=${key}`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+// function showResults() {
+//     alert("Hello There");
+// }
---
+++ js/site2.js
@@ -0,0 +1,107 @@
+'use strict';
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
+}
+
+function getParks(baseUrl, stateArr, maxResults, apiKey) {
+
+    const params = {
+        stateCode: stateArr,
+        limit: maxResults
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = baseUrl + '?' + queryString + '&api_key=' + apiKey;
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => showParks(responseJson, maxResults))
+        .catch(err => {
+            //code here
+            $('.js-error').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+
+function watchForm() {
+        e.preventDefault();
+        const baseUrl = 'https://developer.nps.gov/api/v1/parks'
+        const stateArr = $('#js-state').val().split(",");//code here
+        const maxResults = $('#numResults').val();//code here
+
+        const apiKey = '1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV'
+        getParks(baseUrl, stateArr, maxResults, apiKey);
+}
+
+function showParks(responseJson, maxResults) {
+    let list = document.querySelectorAll(".results-list");
+    console.log(responseJson);
+
+    // $('.error-message').empty();
+    // $('.results-list').empty();
+
+    for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        list.append(`<li>
+      <h3>${responseJson.data[i].fullName}</h3>
+      <a href="${responseJson.data[i].url}" target="blank"><p>${responseJson.data[i].url}</p></a>
+      <p>${responseJson.data[i].description}</p>
+      <p>${responseJson.data[i].directionsInfo} <a href="${responseJson.data[i].directionsUrl}" target="blank">Click for Directions</a></p>
+      
+      </li><br><br>`);
+    }
+    //code here
+    $('#results').removeClass('hidden');
+}
+
+//code here
+$(watchForm);
+
+// ----------------------------------------INSTRUCTION--------------------------------------------------
+
+/* SAMPLE:
+https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV */
+
+// Review The National Parks Services API documentation and create an API key.
+
+
+// Review the API Guide on Authentication for ways to pass your API key as part of the request.
+
+
+// Review the /parks endpoint and data model to understand how it works.
+
+
+// -------------------------------------DISPLAY
+// The parks in the given state must be displayed on the page. Include at least:
+// Full name (fullName)
+// Description (description)
+// Website URL (url)
+// Directions URL (directionsUrl)
+
+// The user must be able to make multiple searches and see only the results for the current search.
+
+// -------------------------------------EXTRA CREDIT
+// As a stretch goal, try adding the park's address to the results. (directionsInfo)
+
+// -------------------------------------API DOCUMENTATION
+// Each API request contains:
+// Resource Endpoint
+// Query String Parameters
+// HTTP Request Header with an API Key
+
+/* For example, consider the following URL:
+https://developer.nps.gov/api/v1/alerts?parkCode=acad,dena
+
+In the above request, two of the three necessary components are represented.
+Resource Endpoint - https://developer.nps.gov/api/v1/alerts
+Query String Parameters - parkCode=acad,dena
+However, just pasting that URL in a browser address bar won't return any results as you also need to send your API key in the HTTP request header. In order to learn more about that, be sure to read the API Guides page about authentication. Also, test out queries and learn more about the API in the API Documentation section of this website.
+
+Good luck with your programming project and thanks for using NPS data. */
